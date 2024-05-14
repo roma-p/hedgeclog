46ead83dd99ac0075438d0d8203020cf50a07dde
roma_p <pelle.romain@gmail.com>
2024-05-14 10:36:10 +0200

    feat(level): defined ZoomLevel object. which defines the size / camera distance for a level.

--- src/common/level.rs
+++ src/common/level.rs
@@ -1,3 +1,4 @@
+
 use bevy::prelude::*;
 
 use crate::config::{StateLevelLoaded, LEVEL_DEFAULT_SIZE, StateUserInputAllowed};
@@ -5,6 +6,56 @@ use crate::common::tiles::EnumeTileBehaviour;
 
 pub const LEVEL_ORIGIN: Vec3 = Vec3::new(0.0, 0.0, 0.0);
 
+pub enum ZOOM_LEVEL {
+    REALLYSMALL = 6,
+    SMALL = 10,
+    NORMAL = 12,
+    BIG = 15,
+    REALLYBIG = 20,
+}
+
+impl ZOOM_LEVEL {
+    pub fn unzoom(&self) -> Option<ZOOM_LEVEL> {
+        match self {
+            ZOOM_LEVEL::REALLYSMALL => Some(ZOOM_LEVEL::SMALL),
+            ZOOM_LEVEL::SMALL => Some(ZOOM_LEVEL::NORMAL),
+            ZOOM_LEVEL::NORMAL => Some(ZOOM_LEVEL::BIG),
+            ZOOM_LEVEL::BIG => Some(ZOOM_LEVEL::REALLYBIG),
+            ZOOM_LEVEL::REALLYBIG => Some(ZOOM_LEVEL::REALLYBIG),
+            _ => None
+        }
+    }
+
+    pub fn zoom(&self) -> Option<ZOOM_LEVEL> {
+        match self {
+            ZOOM_LEVEL::REALLYBIG => Some(ZOOM_LEVEL::BIG),
+            ZOOM_LEVEL::BIG => Some(ZOOM_LEVEL::NORMAL),
+            ZOOM_LEVEL::NORMAL => Some(ZOOM_LEVEL::SMALL),
+            ZOOM_LEVEL::SMALL => Some(ZOOM_LEVEL::REALLYSMALL),
+            ZOOM_LEVEL::REALLYSMALL => Some(ZOOM_LEVEL::REALLYSMALL),
+            _ => None
+        }
+    }
+
+    pub fn get_from_i32(value: i32) -> Option<ZOOM_LEVEL> {
+
+        const I32_REALLYSMALL: i32 = ZOOM_LEVEL::REALLYSMALL as i32;
+        const I32_SMALL: i32 = ZOOM_LEVEL::SMALL as i32;
+        const I32_NORMAL: i32 = ZOOM_LEVEL::NORMAL as i32;
+        const I32_BIG: i32 = ZOOM_LEVEL::BIG as i32;
+        const I32_REALLYBIG: i32 = ZOOM_LEVEL::REALLYBIG as i32;
+
+        match value {
+            I32_REALLYSMALL => Some(ZOOM_LEVEL::REALLYSMALL),
+            I32_SMALL => Some(ZOOM_LEVEL::SMALL),
+            I32_NORMAL=> Some(ZOOM_LEVEL::NORMAL),
+            I32_BIG => Some(ZOOM_LEVEL::BIG),
+            I32_REALLYBIG => Some(ZOOM_LEVEL::REALLYBIG),
+            _ => None
+            // _ => Some(ZOOM_LEVEL::NORMAL)  // DEFAULTING TO ZERO, FIXME: rather panic?
+        }
+    }
+}
 
 #[derive(Resource, Debug, Default)]
 pub struct LevelGrid {
